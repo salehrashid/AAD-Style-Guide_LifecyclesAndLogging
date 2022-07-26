373db762120c3a7e854fc0d071cc7ded55766236
saleh rashid <saleh.rashid.b@gmail.com>
2022-07-26 14:30:22 +0700

    initial commit

---
+++ app/src/main/java/com/app/dessertclicker/ClickerApplication.kt
@@ -0,0 +1,11 @@
+package com.app.dessertclicker
+
+import android.app.Application
+import timber.log.Timber
+
+class ClickerApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
